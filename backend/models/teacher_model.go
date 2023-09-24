c596d7d2f8de8b18de1cca848185bd86629cda29
Fabian Chua <fabianchua6@gmail.com>
2023-09-24 21:55:39 +0800

    feat: update teacher model methods

--- backend/models/teacher_model.go
+++ backend/models/teacher_model.go
@@ -14,6 +14,19 @@ type Teacher struct {
 	Email string    `json:"email"`
 }
 
+func (teacher *Teacher) Create(email string, args ...string) (*Teacher, error) {
+	teacher.Email = email
+	if len(args) > 0 {
+		teacher.Name = args[0]
+	}
+
+	if err := config.DB.Create(&teacher).Error; err != nil {
+		return nil, err
+	}
+
+	return teacher, nil
+}
+
 func (teacher *Teacher) GetAll() ([]Teacher, error) {
 	var teachers []Teacher
 	if err := config.DB.Find(&teachers).Error; err != nil {
@@ -21,4 +34,32 @@ func (teacher *Teacher) GetAll() ([]Teacher, error) {
 	}
 
 	return teachers, nil
+}
+
+func (teacher *Teacher) GetTeacherByID() (*Teacher, error) {
+	if err := config.DB.First(&teacher, teacher.ID).Error; err != nil {
+		return nil, err
+	}
+
+	return teacher, nil
+}
+
+func (teacher *Teacher) UpdateTeacherByID() error {
+	if err := config.DB.Model(&teacher).Where("id = ?", teacher.ID).Updates(map[string]interface{}{
+		"name":  teacher.Name,
+		"email": teacher.Email,
+	}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete teacher by id
+func (teacher *Teacher) DeleteTeacherByID() error {
+	if err := config.DB.Where("id = ?", teacher.ID).Delete(&teacher).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
